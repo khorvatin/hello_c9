dd3f6ac5b30886e05c98efbe0ff4b56f20ac0ebf
Kevin Horvatin <kahorvatin@gmail.com>
2012-02-14 20:06:52 -0600

    Add code to write out request headers

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
 var sys = require("sys");
 var http = require("http");
+var port = 1337;
 
 http.createServer(function(request, response) {
     var d = new Date();
     var dateString = d.toString();
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hello World!<BR>");
+    response.write("Hello World!<BR><BR>");
+    var header;
+    for (header in request.headers)
+    {
+        response.write("<b>request header " + header + ":</b> " + request.headers[header]+"<BR>");
+    }
+    response.write("Hello World!<BR><BR>");
     response.write("Platform: " + process.platform + "<BR>");
     response.write(dateString);
     response.end();
-}).listen(process.env.PORT);
+}).listen(port);
 
-sys.puts("Server running at http://hello_c9.khorvatin.c9.io:"+process.env.PORT+"/");
+sys.puts("Server running at http://localhost:"+port+"/");
